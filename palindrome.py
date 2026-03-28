0e709957b492570f9b0e1812ed53a03f8df88513
abhisek81 <iamabhisek.mohanty@gmail.com>
2026-03-28 23:40:15 +0530

    Palindrome

---
+++ palindrome.py
@@ -0,0 +1,16 @@
+def is_palindrome(x):
+    original = x
+    reversed_num = 0
+    while x > 0:
+        digit = x % 10
+        reversed_num = reversed_num * 10 + digit
+        x = x // 10
+    
+    if original == reversed_num:
+        return "Palindrome"
+    else:
+        return "Not Palindrome"
+    
+x = int(input("Enter an integer : "))
+
+print(is_palindrome(x))
